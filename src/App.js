9024706dc593b490076fe79b0ca24325898e07c2
Aayush <aayushsoni237@gmail.com>
2023-02-22 13:40:11 +0530

    Add category links

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react'
 import NavBar from './components/NavBar'
 import News from './components/News'
 import PropTypes from 'prop-types'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 export default class App extends Component {
   static defaultProps = {
     country: "in",
     pageSize: 5, 
-    category: "general"
+    category: ""
   }
 
   static propTypes = {
@@ -20,10 +25,19 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <Router>
         <NavBar/>
-        <News pageSize={5} country="gb" category='sports' />
+        <Routes>          
+          <Route exact path="/" element={<News key="general" pageSize={5} country="in" category='general' />} />
+          <Route exact path="/entertainment" element={<News key="entertainment" pageSize={5} country="in" category='entertainment' />} />
+          <Route exact path="/sports" element={<News key="sports" pageSize={5} country="in" category='sports' />} />
+          <Route exact path="/business" element={<News key="business"  pageSize={5} country="in" category='business' />} />
+          <Route exact path="/technology" element={<News key="technology" pageSize={5} country="in" category='technology' />} />
+          <Route exact path="/science" element={<News key="science" pageSize={5} country="in" category='science' />} />
+          <Route exact path="/health" element={<News key="health" pageSize={5} country="in" category='health' />} />
+        </Routes>
+        </Router>
       </div>
     )
   }
 }
-
